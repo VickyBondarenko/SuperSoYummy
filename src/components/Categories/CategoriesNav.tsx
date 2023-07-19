878afee441dc5077f171b71eb404fd0b38c0c294
DmytroRudenko11 <dimon11rudenko11@gmail.com>
2023-07-19 22:22:14 +0300

    add grabbing scroll to categories navigation

--- src/components/Categories/CategoriesNav.tsx
+++ src/components/Categories/CategoriesNav.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 
 import { fetchCategories } from "../../redux/categoriesSlice/categoriesThunk";
 import { useAppDispatch, useAppSelector } from "../../hooks/reduxHooks";
@@ -7,6 +7,8 @@ import {
   selectMemoCategoryList,
   // selectIsLoading,
 } from "../../redux/categoriesSlice/categoriesSelector";
+
+import { Draggable } from "../Draggable/Draggable";
 
 interface ICategoriesNavProps {
   handleCategoryChange: (category: string) => void;
@@ -17,30 +19,34 @@ export const CategoriesNav: React.FC<ICategoriesNavProps> = ({
   handleCategoryChange,
   activeCategory,
 }) => {
-  const dispatch = useAppDispatch();
-  const categoryList = useAppSelector(selectMemoCategoryList);
+  const listRef = useRef(null);
 
   useEffect(() => {
     dispatch(fetchCategories());
   }, []);
 
+  const dispatch = useAppDispatch();
+  const categoryList = useAppSelector(selectMemoCategoryList);
+
   return (
-    <div className="w-full h-[55px] overflow-hidden hover:overflow-auto border-b-4 hover:border-b-0 scrollbar-track-listUnderline border-listUnderline scrollbar scrollbar-thumb-accentMain scrollbar-medium mb-8 ">
-      <ul className="flex flex-row gap-[28px] md:gap-[55px] p-2">
-        {categoryList.map((item: string, index: number) => (
-          <li
-            key={index}
-            onClick={() => handleCategoryChange(item)}
-            className={`font-main text-customXxs md:text-[18px] cursor-pointer ${
-              item !== activeCategory
-                ? "text-listUnderline"
-                : "text-accentMain scale-125"
-            } transition duration-300 ease-in-out hover:text-accentMain`}
-          >
-            {item}
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Draggable innerRef={listRef} rootClass={"drag"}>
+      <div className="w-full h-[55px] overflow-hidden hover:overflow-auto border-b-4 hover:border-b-0 scrollbar-track-listUnderline border-listUnderline scrollbar scrollbar-thumb-accentMain scrollbar-medium mb-8 ">
+        <ul className="flex flex-row gap-[28px] md:gap-[55px] p-2">
+          {categoryList.map((item: string, index: number) => (
+            <li
+              key={index}
+              onClick={() => handleCategoryChange(item)}
+              className={`font-main text-customXxs md:text-[18px] cursor-pointer ${
+                item !== activeCategory
+                  ? "text-listUnderline"
+                  : "text-accentMain scale-125"
+              } transition duration-300 ease-in-out hover:text-accentMain`}
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </Draggable>
   );
 };
