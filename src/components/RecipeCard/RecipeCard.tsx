b73ea1b81bcca7db7cc87ddc551bc5e0016bfbc5
DmytroRudenko11 <dimon11rudenko11@gmail.com>
2023-07-21 00:18:54 +0300

    Add default values for decription and time in card overlay

--- src/components/RecipeCard/RecipeCard.tsx
+++ src/components/RecipeCard/RecipeCard.tsx
@@ -15,17 +15,20 @@ export interface IRecipeInfo {
 
 export const RecipeCard: React.FC<IRecipeInfo> = memo((props) => {
   const { preview, title, description, time } = props;
+  const defaultDescription =
+    description.trim() === "" ? "No description" : description;
+  const defaultTime = time.trim() === "" ? "N/A" : `${time} min`;
   return (
     <li className="relative overflow-hidden group">
       <img src={preview} className={styles.recipe_image} />
       <p className={styles.recipe_title}>{title}</p>
       <div className="absolute translate-y-full flex justify-center items-center z-10 top-0 left-0 right-0  h-full  bg-overlayBackdrop rounded-lg p-5 font-main text-whiteText group-hover:translate-y-0 group-hover:opacity-100 transition duration-500 ease-in-out overflow-hidden">
         <p className={styles.recipe_overlay_description}>
-          {truncateText(description)}
+          {truncateText(defaultDescription)}
         </p>
         <p className={styles.recipe_overlay_time}>
           <BsClock />
-          {time} min
+          {defaultTime}
         </p>
       </div>
     </li>
