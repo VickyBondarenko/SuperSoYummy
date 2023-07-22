f3d095fd780def7a83c0a2d6bc39d87e52f8ae03
DmytroRudenko11 <dimon11rudenko11@gmail.com>
2023-07-23 01:12:34 +0300

    add TitlePage components with styling dots

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import styles from "./Categories.module.css";
 
+import { PageTitle } from "../PageTitle/PageTitle";
 import { CategoriesNav } from "./CategoriesNav/CategoriesNav";
 import { CategoriesGallery } from "./CategoriesGallery/CategoriesGallery";
 
 export const Categories: React.FC = () => {
   return (
     <section className={styles.categories_section}>
-      <h1 className={styles.categories_title}>Categories</h1>
+      <PageTitle title={"Categories"} />
       <CategoriesNav />
       <CategoriesGallery />
     </section>
