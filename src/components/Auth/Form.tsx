28dcd77da87bbe6d0d98c85e22962943ebd987b3
VickyBondarenko <quikyv@gmail.com>
2023-07-23 14:53:08 +0200

    Add style for submit button

--- src/components/Auth/Form.tsx
+++ src/components/Auth/Form.tsx
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
 import { Formik, Form, Field } from "formik";
 import { SignupSchema, LoginSchema } from "../../schemas/yupSchemas";
-
-import ErrorSvg from "../../images/svg/authForm/error.svg";
-import SuccessSvg from "../../images/svg/authForm/success.svg";
-// import EmailSvg from "/src/images/svg/authForm/email.svg";
-// import PassSvg from "/src/images/svg/authForm/password.svg";
+import { BsEye, BsEyeSlash } from "react-icons/bs";
+import { ReactComponent as SuccessSvg } from "../../images/svg/authForm/success.svg";
+import { ReactComponent as EmailSvg } from "/src/images/svg/authForm/email.svg";
+import { ReactComponent as PassSvg } from "/src/images/svg/authForm/password.svg";
 // import UserSvg from "/src/images/svg/authForm/name.svg";
 import { ReactComponent as UserSvg } from "/src/images/svg/authForm/name.svg";
 import styles from "./Auth.module.css";
@@ -61,155 +60,100 @@ const AuthForm: React.FC<FormProps> = ({ page, title }) => {
         isValid,
         dirty,
       }) => (
-        <Form className="border-4 border-indigo-500 rounded-[30px] px-[50px] py-[44px] mb-[18px]">
+        <Form className={styles.form_wrapper}>
           <h1 className={styles.form_title}>{title}</h1>
           {page === "register" && (
             <>
-              <label htmlFor="name">
-                <div>
-                  <UserSvg />
-                  {/* <img src={UserSvg} alt="user-svg" /> */}
-                  {/* <UserSvg
-                  //  fill="#fafafa"
-                  // className={`${
-                  //   errors.name && touched.name ? styles.error_fill : ""
-                  // } ${
-                  //   !errors.name && touched.name ? styles.success_fill : ""
-                  // }`}
-                  /> */}
-                </div>
+              <label htmlFor="name" className="relative mb-3 md:mb-6">
+                <UserSvg className={styles.form_svg} />
                 <Field
                   autoComplete="off"
                   type="text"
                   name="name"
                   placeholder="Name"
-                  // className={`${styles.input} ${
-                  //   errors.name && touched.name ? styles.error : ""
-                  // } ${!errors.name && touched.name ? styles.success : ""}`}
                   onChange={handleChange}
                   onBlur={handleBlur}
                   value={values.name}
+                  className={styles.form_input}
                 />
 
                 {touched.name && errors.name && (
-                  <>
-                    <p>{errors.name}</p>
-                    <img src={ErrorSvg} alt="error-svg" />
-                  </>
+                  <p className={styles.form_errorMsg}>{errors.name}</p>
                 )}
                 {touched.name && !errors.name && (
-                  <img src={SuccessSvg} alt="succes-svg" />
+                  <SuccessSvg className={styles.form_svg_success} />
                 )}
               </label>
             </>
           )}
-          <label htmlFor="email">
-            <div>
-              {/* <EmailSvg
-                fill="#fafafa"
-                className={`${
-                  errors.email && touched.email ? styles.error_fill : ""
-                } ${!errors.email && touched.email ? styles.success_fill : ""}`}
-              /> */}
-            </div>
+          <label htmlFor="email" className="relative  mb-3 md:mb-6">
+            <EmailSvg
+              className={`${styles.form_svg} ${
+                touched.email && errors.email
+                  ? "fill-red-600"
+                  : "fill-[#3CBC81]"
+              } `}
+            />
+
             <Field
               autoComplete="off"
               name="email"
               type="email"
               placeholder="Email"
-              // className={`${styles.input} ${
-              //   errors.email && touched.email ? styles.error : ""
-              // } ${!errors.email && touched.email ? styles.success : ""}`}
               onChange={handleChange}
               onBlur={handleBlur}
               value={values.email}
+              className={`${styles.form_input} ${
+                errors.email ? " border-red-600" : "border-whiteText"
+              } ${
+                touched.email && !errors.email
+                  ? " border-[#3CBC81]"
+                  : "border-whiteText"
+              }`}
             />
-            {touched.email && errors.email && (
-              <>
-                <p>{errors.email}</p>
-                <img src={ErrorSvg} alt="error-svg" />
-              </>
+            {errors.email && (
+              <p className={styles.form_errorMsg}>{errors.email}</p>
             )}
-            {touched.name && !errors.name && (
-              <img src={SuccessSvg} alt="succes-svg" />
+            {touched.email && !errors.email && (
+              <SuccessSvg className={styles.form_svg_success} />
             )}
           </label>
-          <label htmlFor="password">
-            <div>
-              {/* <PassSvg
-                fill="#fafafa"
-                className={` ${
-                  errors.password && touched.password ? styles.error_fill : ""
-                } ${
-                  !errors.password && touched.password
-                    ? styles.success_fill
-                    : ""
-                } ${
-                  errors.password?.includes("secure") ? styles.warning_fill : ""
-                }`}
-              /> */}
-            </div>
+          <label htmlFor="password" className="relative  mb-7 md:mb-[50px]">
+            <PassSvg className={styles.form_svg} />
             <Field
               autoComplete="off"
               name="password"
               type={passwordType}
               placeholder="Password"
-              // className={`${styles.input} ${
-              //   errors.password && touched.password ? styles.error : ""
-              // } ${!errors.password && touched.password ? styles.success : ""} ${
-              //   errors.password?.includes("secure") ? styles.warning : ""
-              // }`}
               onChange={handleChange}
               onBlur={handleBlur}
               value={values.password}
+              className={`${styles.form_input} `}
             />
             <div onClick={togglePassword}>
-              {/* {passwordType === "password" ? (
-                <BsEyeSlash fill="#fafafa" />
+              {passwordType === "password" ? (
+                <BsEyeSlash className={styles.form_svg_eye} />
               ) : (
-                <BsEye fill="#fafafa" />
-              )} */}
+                <BsEye className={styles.form_svg_eye} />
+              )}
             </div>
-            {errors.password?.includes("secure") ? (
-              <div>
-                <p> {errors.password}</p>
-                {/* <WorningSvg className={styles.warning_svg} /> */}
-              </div>
-            ) : (
-              touched.password &&
-              errors.password && (
-                <>
-                  <p>{errors.password}</p>
-                  <img src={ErrorSvg} alt="error-svg" />
-                </>
-              )
+            {touched.password && errors.password && (
+              <p className={styles.form_errorMsg}>{errors.password}</p>
             )}
             {touched.password && !errors.password && (
               <>
-                <p>Password is secure</p>
-
-                <img src={SuccessSvg} alt="succes-svg" />
+                <p className={`${styles.form_errorMsg}`}>Password is secure</p>
+                <SuccessSvg className={styles.form_svg_success} />
               </>
             )}
-            {/* {errors.password?.includes('secure') && (
-                        <>
-                          <p className={styles.warning_message}>
-                         
-                            {errors.password}
-                          </p>
-                          <WorningSvg className={styles.worningSvg_svg} />
-                        </>
-                      )} */}
           </label>
           <button
-            // className={styles.submit_button}
             disabled={!isValid && !dirty}
             type="submit"
-            // onClick={() => handleOnSubmit(values)}
+            className={`${styles.form_btn}  bg-accentMain`}
           >
             {page === "signin" ? "Sign in" : "Sign up"}
           </button>
-          {/* Остальной JSX код формы... */}
         </Form>
       )}
     </Formik>
