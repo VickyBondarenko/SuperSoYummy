e7b6aeb34a0c592ffd4cf5656b3c53b37854a343
DmytroRudenko11 <dimon11rudenko11@gmail.com>
2023-07-25 14:47:07 +0300

    fix custom btn types and props

--- src/components/Buttons/AsimetricRoundedBtn.tsx
+++ src/components/Buttons/AsimetricRoundedBtn.tsx
@@ -1,12 +1,17 @@
 interface IBtn {
+  btnType?: "button" | "submit" | "reset";
   text: string;
   style?: string;
 }
 
-export const AsimetricRoundedBtn: React.FC<IBtn> = ({ text, style }) => {
+export const AsimetricRoundedBtn: React.FC<IBtn> = ({
+  text,
+  style,
+  btnType = "button",
+}) => {
   return (
     <button
-      type="button"
+      type={btnType}
       className={`${`px-6 md:px-11  py-[12px] md:py-[22px] font-main text-whiteText text-[14px] md:text-[16px] leading-[21px] md:leading-[24px] border-2 rounded-tl-[24px] rounded-br-[24px] rounded-bl-[44px] rounded-tr-[44px]  `} ${style}`}
     >
       {text}
