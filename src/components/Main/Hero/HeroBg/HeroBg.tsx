9e94a4516c6200f153180e61251d4f02ae544f23
DmytroRudenko11 <dimon11rudenko11@gmail.com>
2023-07-26 00:04:51 +0300

    main page hero tablet in progress

--- src/components/Main/Hero/HeroBg/HeroBg.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./HeroBg.module.css";
-
-export const HeroBg: React.FC = () => {
-  return (
-    <div className={`${styles.hero_angle} ${styles.hero_angle_before}`}></div>
-  );
-};
