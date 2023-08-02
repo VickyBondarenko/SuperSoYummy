a36e00238a3095dd4433c2edbbe64883907da6a4
VickyBondarenko <quikyv@gmail.com>
2023-08-02 22:12:07 +0200

    Add logout for refresh

--- src/api/apiHelpers.tsx
+++ src/api/apiHelpers.tsx
@@ -6,6 +6,13 @@ const VITE_BACKEND_BASE_URL: string = import.meta.env.VITE_BACKEND_BASE_URL;
 
 axios.defaults.baseURL = VITE_BACKEND_BASE_URL;
 
+const handleLogOut = () => {
+  var modal = document.getElementById("myModal");
+  if (modal) {
+    modal.style.display = "block";
+  }
+};
+
 export const setAuthHeader = (token: string) => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
 };
@@ -29,17 +36,16 @@ axios.interceptors.response.use(
         const { data } = await axios.post("api/auth/refresh", {
           refreshToken,
         });
-        console.log("accessToken", data.accessToken);
-        console.log("refreshToken", data.refreshToken);
-        setAuthHeader(data.accessToken);
+
+        // setAuthHeader(data.accessToken);
+        error.config.headers.common.authorization = `Bearer ${data.accessToken}`;
         localStorage.setItem("refreshToken", data.refreshToken);
         return axios(error.config);
       } catch (error) {
         const axiosError = error as AxiosError<SerializedError>;
         if (axiosError.response?.status === 403) {
+          handleLogOut();
           Notify.failure("Session timeout. Login again");
-          // const _id = "64c91efae458c4d71bf9cd72";
-          // axios.post(`/api/auth/logout`, { _id });
         }
 
         return Promise.reject(error);
